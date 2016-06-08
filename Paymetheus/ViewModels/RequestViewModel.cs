b6aebeb78dc2df82489696d06fb8adb80c823ac0
Josh Rickmar <jrick@companyzero.com>
2016-06-08 13:45:05 -0400

    Bind accounts view to the account viewmodels
    Begin binding the account's list item style template to properties of
    the account view model.

--- Paymetheus/ViewModels/RequestViewModel.cs
+++ Paymetheus/ViewModels/RequestViewModel.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) 2016 The Decred developers
 // Licensed under the ISC license.  See LICENSE file in the project root for full license information.
 
-using Paymetheus.Decred.Wallet;
 using Paymetheus.Framework;
 using System;
 using System.Windows;
