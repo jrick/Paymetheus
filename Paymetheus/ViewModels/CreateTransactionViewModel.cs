4eaf5660ffa5364a314fd17dde1b4f0c31fcdff6
Josh Rickmar <jrick@companyzero.com>
2016-06-21 17:12:42 -0400

    cleanup usings

--- Paymetheus/ViewModels/CreateTransactionViewModel.cs
+++ Paymetheus/ViewModels/CreateTransactionViewModel.cs
@@ -8,11 +8,8 @@ using Paymetheus.Decred.Util;
 using Paymetheus.Decred.Wallet;
 using Paymetheus.Framework;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace Paymetheus.ViewModels
