18818486f2ad80046fbb61ce55ac7cff8f1efcee
Marco Peereboom <marco@peereboom.us>
2016-06-29 10:06:38 -0500

    add skeleton unit test for wallet seed

---
+++ Paymetheus.Tests.Decred/Wallet/WalletSeedTests.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) 2016 The btcsuite developers
+// Copyright (c) 2016 The Decred developers
+// Licensed under the ISC license.  See LICENSE file in the project root for full license information.using Paymetheus.Decred.Wallet;
+
+using Paymetheus.Decred.Wallet;
+using Xunit;
+
+namespace Paymetheus.Tests.Decred.Wallet
+{
+    class WalletSeedTests
+    {
+        private static PgpWordList PgpWordList = new PgpWordList();
+
+        [Fact]
+        public static void DecodeAndValidateUserInputTest()
+        {
+            var decodedSeed = WalletSeed.DecodeAndValidateUserInput("moo", PgpWordList);
+        }
+    }
+}
