dd3d2f80a346e02d5134def9363cfb9757338bc7
Marco Peereboom <marco@peereboom.us>
2016-06-29 10:36:36 -0500

    add bunch of postive tests

--- Paymetheus.Tests.Decred/Wallet/WalletSeedTests.cs
+++ Paymetheus.Tests.Decred/Wallet/WalletSeedTests.cs
@@ -2,19 +2,48 @@
 // Copyright (c) 2016 The Decred developers
 // Licensed under the ISC license.  See LICENSE file in the project root for full license information.using Paymetheus.Decred.Wallet;
 
+using Paymetheus.Decred.Util;
 using Paymetheus.Decred.Wallet;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Paymetheus.Tests.Decred.Wallet
 {
-    class WalletSeedTests
+    public static class WalletSeedTests
     {
         private static PgpWordList PgpWordList = new PgpWordList();
 
-        [Fact]
-        public static void DecodeAndValidateUserInputTest()
+        public static IEnumerable<object[]> PositiveTests()
         {
-            var decodedSeed = WalletSeed.DecodeAndValidateUserInput("moo", PgpWordList);
+            return new[]
+            {
+                new object[] {
+                    "497497071bdbdf3fccdfddcf828dd18aac4493eda269253753f99897b84fd688",
+                    "deckhand hydraulic preshrunk amusement beeswax suspicious talon customer spigot therapist swelter Saturday miser microscope stairway maverick ribcage designing playhouse unify rebirth guitarist bombast consensus dwelling Waterloo printer mosquito select document stockman maritime spearhead",
+                },
+                new object[] {
+                    "497497071bdbdf3fccdfddcf828dd18aac4493eda269253753f99897b84fd688",
+                    "deckhand hydraulic preshrunk amusement beeswax suspicious talon customer spigot therapist swelter Saturday miser microscope stairway maverick ribcage designing playhouse unify rebirth guitarist bombast consensus dwelling Waterloo printer mosquito select document stockman maritime",
+                },
+                new object[] {
+                    "497497071bdbdf3fccdfddcf828dd18aac4493eda269253753f99897b84fd688",
+                    "497497071bdbdf3fccdfddcf828dd18aac4493eda269253753f99897b84fd688",
+                },
+                new object[] {
+                    "497497071bdbdf3fccdfddcf828dd18aac4493eda269253753f99897b84fd688",
+                    "497497071bdbdf3fccdfddcf828dd18aac4493eda269253753f99897b84fd68800",
+                },
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(PositiveTests))]
+        public static void DecodeAndValidateUserInputTest(string seed, string pgpSeed)
+        {
+            var decodedSeed = WalletSeed.DecodeAndValidateUserInput(seed, PgpWordList);
+            var decodedSeedHex = Hexadecimal.Decode(seed);
+
+            Assert.Equal(decodedSeedHex, decodedSeed);
         }
     }
 }
